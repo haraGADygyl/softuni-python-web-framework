0bcff0be02438df66f7d01b1c19f259a8c1b7566
haraGADygyl <ti60gtt@gmail.com>
2022-03-05 21:25:04 +0200

    initial commit django101

---
+++ django_101/django_101/web/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+# pure python func
+# - called with a django request object
+# - returns django response object
+def index(request):
+    pass
